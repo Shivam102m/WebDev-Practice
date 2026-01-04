9256a1f5e6c399d41bf562d6894aedc8a3ba2ae9
Shivam <moreyshivam00@gmail.com>
2026-01-04 22:20:15 +0530

    Pattern 6-9 added.

--- Cpp pract/Pattern-pract.cpp
+++ Cpp pract/Pattern-pract.cpp
@@ -2,6 +2,62 @@
 
 using namespace std;
 
+void Pattern_9(){
+  int n=5;
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+      cout << "  ";
+    }
+    for(int k=0; k<2*i+1;k++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }  
+  for(int i=n; i>0;i--){
+    for(int j=0 ; j<n-i; j++){
+      cout<<"  ";
+    }
+    for(int k=0; k<2*i-1;k++){
+      cout<<"* ";
+    }cout<<endl;
+  }
+}
+
+void Pattern_8(){
+  int n=5;
+  for(int i=n; i>0;i--){
+    for(int j=0 ; j<n-i; j++){
+      cout<<"  ";
+    }
+    for(int k=0; k<2*i-1;k++){
+      cout<<"* ";
+    }cout<<endl;
+  }
+}
+
+void Pattern_7(){
+  int n=5;
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+      cout << "  ";
+    }
+    for(int k=0; k<2*i+1;k++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }  
+}
+
+void Pattern_6(){
+  int n=5;
+  for(int i=n; i>=0;i--){
+    for(int j=0; j<i; j++){
+      cout<<j+1<<" ";
+    }
+    cout<<endl;
+  }
+}
+
 void Pattern_5(){
   int n=5;
   for(int i=n; i>=0; i--){
@@ -53,6 +109,6 @@ void Pattern_1(){
   }
 }
 int main(){
-  Pattern_5();
+  Pattern_9();
   return 0;
 }
